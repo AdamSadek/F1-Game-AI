2abae8f4f0dfd05a23136633d234bdf9352deec2
Adam Sadek <adam.sadekk@gmail.com>
2023-07-11 14:35:03 +0100

    Resolving some eslint errors

--- drivers.py
+++ drivers.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring,missing-class-docstring
+# pylint: disable=missing-module-docstring,missing-class-docstring,disable=too-few-public-methods
 class Driver:
     def __init__(self, color, position, speed):
         self.color = color
--- f1_main.py
+++ f1_main.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-module-docstring,missing-class-docstring,import-error
 import sys
 import pygame
 from drivers import Driver
